8dbf6c5654616589fcc9626c411a080062cb8ade
Jon Sundin <sundinjon@gmail.com>
2022-12-05 12:59:43 -0800

    updated Table bindings parse

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -16,34 +16,37 @@ const Table = (props) => {
   const parseBinding = (binding) => {
     return (
       <tr key={uuid()}>
-        <td key={uuid()}><a href={binding.parent.value}>{binding.parent.value}</a></td>
-        <td key={uuid()}>{binding.parentLabel.value}</td>
-        <td key={uuid()}>{binding.child.value}</td>
+        <td key={uuid()}>
+          <a href={binding.child.value}>{binding.child.value}</a>
+        </td>
         <td key={uuid()}>{binding.childLabel.value}</td>
-        <td key={uuid()}>{binding.grandChild.value}</td>
+        <td key={uuid()}>
+          <a href={binding.grandChild.value}>{binding.grandChild.value}</a>
+        </td>
         <td key={uuid()}>{binding.grandChildLabel.value}</td>
+        <td key={uuid()}>
+          <a href={binding.greatGrandChild.value}>{binding.greatGrandChild.value}</a>
+        </td>
+        <td key={uuid()}>{binding.greatGrandChildLabel.value}</td>
       </tr>
-    )
+    );
   };
-
 
   let table = null;
   if (data !== null && data !== undefined && Object.keys(data).length > 0) {
     table = (
-      <table style={{display: "table"}}>
+      <table style={{ display: "table" }}>
         <thead>
           <tr>
-            <th>Parent URI</th>
-            <th>Parent Node</th>
             <th>Child URI</th>
             <th>Child Node</th>
             <th>Grandchild URI</th>
             <th>Grandchild Node</th>
+            <th>Great Grandchild URI</th>
+            <th>Great Grandchild Node</th>
           </tr>
         </thead>
-        <tbody>
-          {bindings.map(binding => parseBinding(binding))}
-        </tbody>
+        <tbody>{bindings.map((binding) => parseBinding(binding))}</tbody>
       </table>
     );
   }
