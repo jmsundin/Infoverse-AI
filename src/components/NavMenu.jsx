7cce11e61573a0f416e70ff4c176bbfd3010207e
Jon Sundin <sundinjon@gmail.com>
2023-03-09 10:05:57 -0800

    added Hamburger menu to NavMenu

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,19 +1,46 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React, { useState } from "react";
+import HamburgerMenu from "./HamburgerMenu";
+
+import "../assets/NavMenu.css";
 
 const NavMenu = () => {
-  return (
-    <>
-      <Navbar bg="light" variant="light">
-        <Container>
-          <Navbar.Brand href="#home">Visualize Wikidata</Navbar.Brand>
-          <Nav className="me-auto">
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
+  const headerLogo = (
+    <div className="main-header__logo">
+      <a href="/" alt="Infoverse AI">
+        Infoverse AI
+      </a>
+    </div>
   );
-}
+
+  const menuItems = (
+    <ul className="main-nav__items">
+      <li className="main-nav__item">
+        <a href="/" alt="Home">
+          Home
+        </a>
+      </li>
+      <li className="main-nav__item">
+        <a href="/about" alt="About">
+          About
+        </a>
+      </li>
+      <li className="main-nav__item">
+        <a href="/contact" alt="Contact">
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
+
+  return (
+    <header className="main-header">
+      {headerLogo}
+      <nav className="main-nav-container">
+        {HamburgerMenu}
+        {menuItems}
+      </nav>
+    </header>
+  );
+};
 
 export default NavMenu;
