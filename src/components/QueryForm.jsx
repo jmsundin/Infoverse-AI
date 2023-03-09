0efe3bd7badcd27dd7520ed6c0c9f914aeaae71c
Jon Sundin <sundinjon@gmail.com>
2023-03-09 10:11:41 -0800

    adding search functionality to form

--- src/components/QueryForm.jsx
+++ src/components/QueryForm.jsx
@@ -1,139 +1,137 @@
-import { useState, useRef } from "react";
-import Select from 'react-select';
+import React, { useState, useEffect, useRef } from "react";
+import Select from "react-select";
+
 import { SPARQLQueryDispatcher } from "../utils/SPARQLQuery/SPARQLQueryDispatcher";
 import queryData from "../utils/SPARQLQuery/queryData";
 
-import "../assets/Dropdown.css";
+import "../assets/QueryForm.css";
 
 const QueryForm = (props) => {
-  const [chosenChart, setChosenChart] = useState("Network Diagram");
-  const [chosenTopic, setChosenTopic] = useState("Computer Science");
+  const [chosenChart, setChosenChart] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
   const [chosenProperty, setChosenProperty] = useState("Subclass of");
+
+  // let wikidataSearchURL =
+  //   "https://www.wikidata.org/w/api.php?action=wbsearchentities&format=json&type=item&language=en&search=" +
+  //   inputValue;
 
   const wikidataItems = queryData.wikidataItems;
   const wikidataProperties = queryData.wikidataProperties;
 
-  const topicSelectOptions = queryData.topics.map((topic) => {
-    return { value: topic, label: topic };
-    });
-
   let query = `SELECT ?child ?childLabel ?grandChild ?grandChildLabel ?greatGrandChild ?greatGrandChildLabel WHERE {
-    ?child wdt:${wikidataProperties[chosenProperty]} wd:${wikidataItems[chosenTopic]} .
+    ?child wdt:${wikidataProperties[chosenProperty]} wd:${wikidataItems[inputValue]} .
     ?grandChild wdt:${wikidataProperties[chosenProperty]} ?child .
     ?greatGrandChild wdt:${wikidataProperties[chosenProperty]} ?grandChild .
     SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en,en"  }  
   }
   LIMIT 1000`;
 
-  const onSubmitHandler = (event) => {
+  function onSubmitHandler(event){
     event.preventDefault();
-    const queryDispatcher = new SPARQLQueryDispatcher(queryData.endpointUrl);
-    queryDispatcher.query(query).then((resource) => {
-      props.onQuerySubmit(resource, chosenChart, chosenTopic, chosenProperty);
-    });
+    if (chosenChart.length !== 0 && inputValue.length !== 0) {
+      const queryDispatcher = new SPARQLQueryDispatcher(queryData.endpointUrl);
+      queryDispatcher.query(query).then((resource) => {
+        props.onQuerySubmit(resource, chosenChart, inputValue, chosenProperty);
+      });
+    } else {
+      // alert("Please select a chart and a topic");
+    }
   };
 
-  const onClickTestHandler = (event) => {
-    props.onQuerySubmit(chosenChart, chosenTopic, chosenProperty);
+  // send user input on each key stroke (anytime the inputValue changes)
+  useEffect(() => {
+    fetch(
+      `https://www.wikidata.org/w/api.php?action=wbsearchentities&format=json&language=en&type=item&limit=10&search=${inputValue}`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((searchResults) => {
+        console.log("Search Results:", searchResults);
+        setSuggestions(
+          searchResults.search.map((item) => ({
+            label: item.label,
+            id: item.id,
+          }))
+        );
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation:", error);
+      });
+  }, [inputValue]);
+
+  function handleInputChange(event) {
+    setInputValue(event.target.value);
   };
+
+  function handleSelect(item){
+    setInputValue(item.id);
+  };
+
+  const datalistOptions = queryData.topics.map((topic) => {
+    return <option key={topic} name={topic} value={topic} />;
+  });
+
+  const suggestionOptions = suggestions.map((item) => (
+    <option key={item.id} onClick={() => handleSelect(item)}>
+      {item.label}
+    </option>
+  ));
 
   const chartDropdownMenu = (
-    <>
-      <label>Pick a Chart Type</label>
-      <br></br>
-      <div className="dropdown">
-        <button className="dropdownbtn" type="text">
-          {chosenChart}
-        </button>
-        <div className="dropdown-content">
-          {queryData.charts.map((chart) => (
-            <button
-              key={chart}
-              name={chart}
-              onClick={(event) => setChosenChart(event.target.name)}
-            >
-              {chart}
-            </button>
-          ))}
-        </div>
-      </div>
-    </>
+    <div className="chart-menu">
+      <button className="chart-menu__button">
+        {chosenChart.length > 0 ? chosenChart : "Select a Chart"}
+      </button>
+      <ul className="chart-menu__dropdown">
+        {queryData.charts.map((chart) => (
+          <li
+            key={chart}
+            onClick={() => setChosenChart(chart)}
+          >
+            {chart}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 
-  // TODO: add searchable Select component
-  const topicSelectMenu = (
-    <>
-    {/* <Select options={topicSelectOptions} /> */}
-      <label htmlFor="dropdown-div">Select a Topic to Explore</label>
-      <br></br>
-      <div id="drowdown-div" className="dropdown">
-        <button className="dropdownbtn" type="text">
-          {chosenTopic}
-        </button>
-        <div className="dropdown-content">
-          {queryData.topics.map((topic) => (
-            <button
-              key={topic}
-              name={topic}
-              onClick={(event) => setChosenTopic(event.target.name)}
-            >
-              {topic}
-            </button>
-          ))}
-        </div>
-      </div>
-    </>
-  );
-
-  const propertyDropdownMenu = (
-    <>
-      <label htmlFor="dropdown-div">Select a Property</label>
-      <br></br>
-      <div id="drowdown-div" className="dropdown">
-        <button className="dropdownbtn" type="text">
-          {chosenProperty}
-        </button>
-        <div className="dropdown-content">
-          {queryData.properties.map((property) => (
-            <button
-              key={property}
-              name={property}
-              onClick={(event) => setChosenProperty(event.target.name)}
-            >
-              {property}
-            </button>
-          ))}
-        </div>
-      </div>
-    </>
+  const search = (
+    <div className="topic-search">
+      <input
+        type="search"
+        value={inputValue}
+        onChange={handleInputChange}
+        list="suggestion-options"
+        placeholder="Search"
+        className="topic-search__input"
+        size="25"
+      />
+      <datalist id="suggestion-options">
+        {suggestions.length === 0 ? datalistOptions : suggestionOptions}
+      </datalist>
+    </div>
   );
 
   const exploreButton = (
-    <button className="dropdownbtn" onClick={onSubmitHandler}>
-      Explore
-    </button>
-  );
-
-  const testButton = (
-    <button className="dropdownbtn" onClick={onClickTestHandler}>
-      Test
-    </button>
+    <input
+      type="submit"
+      value="Explore"
+      className="explore-button"
+      onClick={onSubmitHandler}
+    />
   );
 
   return (
-    <table style={ { margin: "auto" }}>
-      <tbody>
-        <tr>
-          <td>{chartDropdownMenu}</td>
-          <td>{topicSelectMenu}</td>
-          <td>{propertyDropdownMenu}</td>
-          <td>
-            <br></br>
-            {exploreButton}
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <form onSubmit={onSubmitHandler} className="query-form">
+      {chartDropdownMenu}
+      {search}
+      {exploreButton}
+    </form>
   );
 };
 
