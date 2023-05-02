9f552cd5c21323bd76e9e70791080e33c9d77cb2
Jon Sundin <sundinjon@gmail.com>
2023-05-01 22:47:43 -0700

    moved QueryForm, added useDebounceCallback custom hook for search, removed Explore btn, added search and submit handlers for Enter key and select item events, added fuzzy search match

---
+++ src/components/NavBar/QueryForm.jsx
@@ -0,0 +1,247 @@
+import React, { useState, useEffect, useMemo, useCallback } from "react";
+import axios from "axios";
+import debounce from "lodash.debounce";
+import { wikidata } from "../../data/queryData";
+import { objectToGraphData } from "../../utils/transformData";
+import DatalistInput from "react-datalist-input";
+
+import "./QueryForm.css";
+import NetworkClient from "../../utils/NetworkClient";
+
+const levenshteinDistance = (a, b) => {
+  if (a.length === 0) return b.length;
+  if (b.length === 0) return a.length;
+
+  const matrix = [];
+
+  // increment along the first column of each row
+  let i;
+  for (i = 0; i <= b.length; i++) {
+    matrix[i] = [i];
+  }
+
+  // increment each column in the first row
+  let j;
+  for (j = 0; j <= a.length; j++) {
+    matrix[0][j] = j;
+  }
+
+  // Fill in the rest of the matrix
+  for (i = 1; i <= b.length; i++) {
+    for (j = 1; j <= a.length; j++) {
+      const cost = a.charAt(j - 1) === b.charAt(i - 1) ? 0 : 1;
+
+      matrix[i][j] = Math.min(
+        matrix[i - 1][j] + 1, // deletion
+        matrix[i][j - 1] + 1, // insertion
+        matrix[i - 1][j - 1] + cost // substitution
+      );
+    }
+  }
+
+  return matrix[b.length][a.length];
+};
+
+const basicFuzzySearch = (inputValue, searchOptionTopics) => {
+  let bestMatch = null;
+  let minDistance = Infinity;
+
+  for (const topic of searchOptionTopics) {
+    const distance = levenshteinDistance(inputValue, topic);
+
+    if (distance < minDistance) {
+      minDistance = distance;
+      bestMatch = topic;
+    }
+  }
+
+  return bestMatch;
+};
+
+const createOptionItem = (topic) => ({
+  id: topic.id,
+  value: topic.value || topic.label,
+  node: (
+    <>
+      <b>{topic.value || topic.label}</b>
+      <br />
+      <span>{topic.description}</span>
+    </>
+  ),
+});
+
+const useDebouncedCallback = (callback, delay, dependencies) => {
+  const debouncedFn = useCallback(debounce(callback, delay), dependencies);
+
+  useEffect(() => {
+    return () => {
+      debouncedFn.cancel();
+    };
+  }, [debouncedFn]);
+
+  return debouncedFn;
+};
+
+const QueryForm = (props) => {
+  const [searchOptions, setSearchOptions] = useState(wikidata.suggestedTopics);
+  const [searchOptionItems, setSearchOptionItems] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [selectedTopic, setSelectedTopic] = useState({
+    id: null,
+    value: null,
+    label: null,
+  });
+
+  useEffect(() => {
+    setSearchOptionItems(searchOptions.map(createOptionItem));
+  }, [searchOptions]);
+
+  useEffect(() => {
+    const items = searchOptions.map((topic) => {
+      return {
+        id: topic.id,
+        value: topic.value || topic.label,
+        node: (
+          <>
+            <b>{topic.value || topic.label}</b>
+            <br />
+            <span>{topic.description}</span>
+          </>
+        ),
+      };
+    });
+    setSearchOptionItems(items);
+  }, [searchOptions]);
+
+  const handleSearchInput = useCallback(
+    async (event) => {
+      const input = event.target.value;
+      console.log(input);
+      if (!input) return;
+      setInputValue(input);
+
+      try {
+        const response = await axios.get("https://www.wikidata.org/w/api.php", {
+          params: {
+            action: "wbsearchentities",
+            format: "json",
+            language: "en",
+            limit: 10,
+            origin: "*",
+            search: input,
+          },
+        });
+
+        const fetchedOptions = response.data.search;
+        setSearchOptions(fetchedOptions);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    [inputValue]
+  );
+
+  const handleDebounceSearchInput = useDebouncedCallback(
+    handleSearchInput,
+    300,
+    [inputValue]
+  );
+
+  // Stop the invocation of the debounced function
+  // after unmounting
+  useEffect(() => {
+    return () => {
+      handleDebounceSearchInput.cancel();
+    };
+  }, []);
+
+  const findClosestMatch = (inputValue, searchOptions) => {
+    const bestMatch = basicFuzzySearch(
+      inputValue,
+      searchOptions.map((option) => {
+        return option.label;
+      })
+    );
+
+    if (bestMatch) {
+      return searchOptions.find((option) => option.label === bestMatch);
+    }
+
+    return null;
+  };
+
+  const submitHandler = useCallback(
+    (topicObj) => {
+      let topicToSubmit = topicObj;
+      if (topicObj.label) {
+        topicToSubmit = findClosestMatch(topicObj.label, searchOptions);
+        if (!topicToSubmit) return;
+      }
+
+      const endpoint = "https://query.wikidata.org/sparql";
+      const sparqlQuery = wikidata.propertyQueries.subClassOf.replaceAll(
+        "Q21198",
+        topicToSubmit.id
+      );
+
+      NetworkClient.sparqlQuery(endpoint, sparqlQuery, (data) => {
+        const root = {
+          ...topicToSubmit,
+          value: data?.results?.bindings?.length,
+        };
+
+        const graph = objectToGraphData(root, data?.results?.bindings);
+        props.onQuerySubmit(graph);
+      });
+    },
+    [selectedTopic, inputValue, searchOptions]
+  );
+
+  const onTopicSelectedHandler = useCallback(
+    (topic) => {
+      const selectedTopicObj = { id: topic.id, value: 1, label: topic.value };
+      setSelectedTopic(selectedTopicObj);
+      submitHandler(selectedTopicObj);
+    },
+    [selectedTopic]
+  );
+
+  const onKeyDownHandler = (event) => {
+    if (event.key === "Enter") {
+      const input = event.target.value;
+      if (input) {
+        setInputValue(input);
+        onSubmitHandler(event);
+      }
+    } else if (event.key) {
+      handleDebounceSearchInput(event);
+    } else if (event.key === "Escape") {
+      setInputValue("");
+    }
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    const input = event.target.value;
+    if (input) {
+      setInputValue(input);
+      submitHandler({ id: null, value: 1, label: input });
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmitHandler} className="query-form">
+      <DatalistInput
+        className="react-datalist-input"
+        label={false}
+        placeholder="Search for a topic"
+        value={inputValue}
+        items={searchOptionItems}
+        onSelect={onTopicSelectedHandler}
+        onKeyDown={onKeyDownHandler}
+      />
+    </form>
+  );
+};
+
+export default QueryForm;
