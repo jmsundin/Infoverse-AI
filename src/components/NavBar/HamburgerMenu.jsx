b4e0262e6a04f87961d77dee954e323499fc7b4f
Jon Sundin <sundinjon@gmail.com>
2023-05-01 23:11:51 -0700

    moved to NavBar component folder, not using this component anymore

---
+++ src/components/NavBar/HamburgerMenu.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import "../assets/NavMenu.css";
+import "../assets/HamburgerMenu.css";
+
+const HamburgerMenu = (props) => {
+  const spanHamburgerMenu = (
+    <div className="hamburger-menu">
+      <span className="bar"></span>
+      <span className="bar"></span>
+      <span className="bar"></span>
+      <span className="bar"></span>
+    </div>
+  );
+
+  const svgHamburgerMenu = (
+    <svg viewBox="0 0 24 24" width="24" height="24">
+      <line
+        x1="0"
+        y1="1"
+        x2="28"
+        y2="1"
+        style={{ stroke: "#fff", strokeWidth: "2.5" }}
+      />
+      <line
+        x1="0"
+        y1="8"
+        x2="28"
+        y2="8"
+        style={{ stroke: "#fff", strokeWidth: "2.5" }}
+      />
+      <line
+        x1="0"
+        y1="15"
+        x2="28"
+        y2="15"
+        style={{ stroke: "#fff", strokeWidth: "2.5" }}
+      />
+    </svg>
+  );
+
+  const crossIcon = (
+    <svg viewBox="0 0 21 21" width="24" height="24">
+      <line
+        x1="1"
+        y1="1"
+        x2="20"
+        y2="20"
+        style={{ stroke: "#fff", strokeWidth: "2.5" }}
+      />
+      <line
+        x1="1"
+        y1="20"
+        x2="20"
+        y2="0"
+        style={{ stroke: "#fff", strokeWidth: "2.5" }}
+      />
+    </svg>
+  );
+
+  return (
+    {svg: svgHamburgerMenu, span: spanHamburgerMenu, cross: crossIcon}
+  );
+};
+
+export default HamburgerMenu;
