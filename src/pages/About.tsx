06a746b9af0d0f05a6e8e8d9444913e0afd566af
Jon Sundin <sundinjon@gmail.com>
2023-05-30 11:52:19 -0700

    moved files to client directory, added gradient, updated about page

---
+++ src/pages/About.tsx
@@ -0,0 +1,74 @@
+import { useState, Fragment } from "react";
+
+import NavBar from "../components/NavBar/NavBar";
+import "./About.css";
+
+const About = () => {
+  const [page, setPage] = useState("About");
+
+  return (
+    <Fragment>
+      <NavBar page={page} />
+      <div className="gradient__bg about-section">
+        <div className="header__content card">
+          <h2>About Infoverse AI</h2>
+          <p>
+            &emsp; &emsp; Infoverse AI helps you visualize and explore the
+            relations between topics. The data comes from{" "}
+            <a href="https://wikidata.org">Wikidata.org</a> and{" "}
+            <a href="https://wikipedia.org">Wikipedia.org</a>, sister projects
+            of <a href="https://wikimedia.org">Wikimedia.org</a>.
+          </p>
+        </div>
+      </div>
+      <div className="gradient__bg how-to-section">
+        <div className="header__content card">
+          <h2>How to Use Infoverse AI:</h2>
+          <p>
+            <ol>
+              <li>
+                You can pick a suggested topic by clicking on the Search bar, or
+                you can Search for a Topic.
+              </li>
+              <li>
+                Topics in the visualization can be clicked to see their
+                relations.
+              </li>
+            </ol>
+          </p>
+        </div>
+      </div>
+      <div className="gradient__bg technologies-section">
+        <div className="header__content card">
+          <h2>Technologies Used in this Site:</h2>
+          <p>
+            <ul>
+              <li>
+                Frontend JavaScript Library:{" "}
+                <a href="https://reactjs.org/">React</a>
+              </li>
+              <li>
+                Wikidata SPARQL Endpoint:{" "}
+                <a href="https://query.wikidata.org/">SPARQL</a>
+              </li>
+              <li>
+                Network Diagram: <a href="https://visjs.github.io/vis-network/docs/network/">Vis.js's Vis-Network</a>
+              </li>
+              <li>
+                Data Driven Documents (D3):{" "}
+                <a href="https://d3js.org/">D3.js</a>
+              </li>
+              <li>
+                Data sources: <a href="https://wikimedia.org">Wikimedia.org</a>,{" "}
+                <a href="https://wikidata.org">Wikidata.org</a>, and{" "}
+                <a href="https://wikipedia.org">Wikipedia.org</a>
+              </li>
+            </ul>
+          </p>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default About;
---
+++ src/pages/Contact.tsx
@@ -0,0 +1,65 @@
+import { useState, Fragment } from "react";
+import NavBar from "../components/NavBar/NavBar";
+import "./Contact.css";
+
+import profilePic from "../assets/me-profile1-bg-200x267.jpeg";
+
+const ContactPage = () => {
+  const [page, setPage] = useState("Contact");
+
+  return (
+    <Fragment>
+      <NavBar page={page} />
+      <div className="gradient__bg contact-section">
+        <div className="contact-section__header">
+          <div className="card">
+            <img
+              src={profilePic}
+              width="200"
+              height="200"
+              className="profile-pic"
+              alt="A picture of Jon Sundin"
+            />
+            <ul className="contact-section__list">
+              <li className="contact-section__list--item">
+                <h1>Jon Sundin</h1>
+              </li>
+              <li className="contact-section__list--item">
+                <h3>Software Engineer</h3>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="gradient__bg contact-section__footer">
+          <div className="card">
+            <div>
+              <strong>E-Mail: </strong>
+              jon [at] infoverse [dot] ai
+            </div>
+            <div>
+              <strong>LinkedIn: </strong>
+              <a href="https://www.linkedin.com/in/jonsundin/" target="_blank">
+                Jon Sundin
+              </a>
+            </div>
+
+            <div>
+              <strong>GitHub: </strong>
+              <a href="https://github.com/jmsundin" target="_blank">
+                jmsundin
+              </a>
+            </div>
+            <div>
+              <strong>Twitter: </strong>
+              <a href="https://twitter.com/JonSundin" target="_blank">
+                @JonSundin
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default ContactPage;
